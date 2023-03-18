9dd380fc57ebef3e837d43569bce6fca7da5b79c
shengdli <124101883+shengdli@users.noreply.github.com>
2023-03-18 14:39:57 -0400

    test insert

--- s.py
+++ s.py
@@ -3,13 +3,14 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="911Apexpredator"
+  password="dd020912#"
 )
 
 if mydb.is_connected():
     print("Connection Established")
 mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM musicapp.users")
+mycursor.execute("SELECT * FROM MusicApp.users")
 results = mycursor.fetchall()
 print(results)
-mycursor.execute("insert into users (user_id,email,password) values (2,002@bu.edu,123)")
+mycursor.execute('''INSERT INTO MusicApp.users (user_id, email, password) VALUES (%s, %s, %s)''', (3, "003@bu.edu", 1234))
+mydb.commit()
