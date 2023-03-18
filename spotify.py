cdf8182d0a8a88e544d4688f48b2052c3d46b0cb
KitYan20 <112986746+KitYan20@users.noreply.github.com>
2023-03-18 16:56:40 -0400

    fixed syntax errors

--- spotify.py
+++ spotify.py
@@ -2,7 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 # taylor_uri = 'spotify:artist:06HL4z0CvFAxyc27GXpf02'
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="e825b2eb9d73456fb74b50108dd1f8a3", client_secret="f06de69fc67c4ce185cb5deea4748141"))
+spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="e825b2eb9d73456fb74b50108dd1f8a3", client_secret="f06de69fc67c4ce185cb5deea4748141"))
 
 # results = spotify.artist_top_tracks(taylor_uri, 'US')
 # albums = results['items']
@@ -15,21 +15,21 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="e825b2eb9d
 def Playlist():
 #You can change the name to any artist you want
     artist_name = 'BLACKPINK'
-    results = sp.search(q='artist:' + artist_name, type='artist')
+    results = spotify.search(q='artist:' + artist_name, type='artist')
     artist_id = results['artists']['items'][0]['id']
 
     # get list of artist's albums
     albums = []
-    results = sp.artist_albums(artist_id, album_type='album')
+    results = spotify.artist_albums(artist_id, album_type='album')
     albums.extend(results['items'])
     while results['next']:
-        results = sp.next(results)
+        results = spotify.next(results)
         albums.extend(results['items'])
 
     # get list of artist's tracks
     tracks = []
     for album in albums:
-        results = sp.album_tracks(album['id'])
+        results = spotify.album_tracks(album['id'])
         tracks.extend(results['items'])
 
     list =[]
@@ -41,15 +41,15 @@ def Playlist():
 
 def singleAlbum():
     artist_name = 'BLACKPINK'
-    results = sp.search(q='artist:' + artist_name, type='artist')
+    results = spotify.search(q='artist:' + artist_name, type='artist')
     artist_id = results['artists']['items'][0]['id']
 
     # get list of artist's albums
     albums = []
-    results = sp.artist_albums(artist_id, album_type='album')
+    results = spotify.artist_albums(artist_id, album_type='album')
     albums.extend(results['items'])
     while results['next']:
-        results = sp.next(results)
+        results = spotify.next(results)
         albums.extend(results['items'])
     
     album_name = 'Born Pink'
@@ -57,7 +57,7 @@ def singleAlbum():
     for album in albums:
         if album['name'].lower() == album_name.lower():
             album_id = album['id']
-            album_details = sp.album(album_id)
+            album_details = spotify.album(album_id)
             break
 
 # print album details
@@ -65,6 +65,7 @@ def singleAlbum():
     print('Artist:', album_details['artists'][0]['name'])
     print('Release date:', album_details['release_date'])
     print('Total tracks:', album_details['total_tracks'])
+    return 
 print(len(Playlist()))
 print(singleAlbum())
 
