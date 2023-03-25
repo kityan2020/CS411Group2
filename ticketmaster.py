8130aea83bc7cffd2fa5a13a00074e3ff2c60aad
KitYan20 <112986746+KitYan20@users.noreply.github.com>
2023-03-25 12:10:54 -0400

    created a ticketmaster api

---
+++ ticketmaster.py
@@ -0,0 +1,29 @@
+import requests
+# define API endpoint and parameters
+url = 'https://app.ticketmaster.com/discovery/v2/events.json'
+api_key = 'qjdg4sTDUGRKNiGaLzjfxwoo3MoSBv0y'
+#we can get these from user input and through location
+artist_name = 'The Weeknd'
+location = 'New York'
+
+# make GET request to API endpoint with parameters and API key
+response = requests.get(url, params={
+    'apikey': api_key,
+    'keyword': artist_name,
+    'city': location
+})
+if response.status_code == 200:
+    events = response.json()
+    if '_embedded' in events and 'events' in events['_embedded']:
+        events = events['_embedded']['events']
+        for event in events:
+            print('Name:', event['name'])
+            print('Date:', event['dates']['start']['localDate'])
+            #print('Time:', event['dates']['start']['localTime'])
+            print('Venue:', event['_embedded']['venues'][0]['name'])
+            print('Location:', event['_embedded']['venues'][0]['city']['name'])
+            print('------------------------------')
+    else:
+        print('No events found')
+else:
+    print('Error:Request failed with status code', response.status_code)
