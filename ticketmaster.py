9cbb52cf8403c4ef1530c95cee9a1d93dbec61be
KitYan20 <112986746+KitYan20@users.noreply.github.com>
2023-04-07 13:04:51 -0400

    Change the parameter to a user input of artist

--- ticketmaster.py
+++ ticketmaster.py
@@ -3,28 +3,34 @@ import requests
 url = 'https://app.ticketmaster.com/discovery/v2/events.json'
 api_key = 'qjdg4sTDUGRKNiGaLzjfxwoo3MoSBv0y'
 #we can get these from user input and through location
-artist_name = input('Artist: ')
-location =  input('Location: ')
+# artist_name = input('Artist: ')
+# location =  input('Location: ')
+def events(artist_name):
+    # make GET request to API endpoint with parameters and API key
+    response = requests.get(url, params={
+        'apikey': api_key,
+        'keyword': artist_name
+    })
 
-# make GET request to API endpoint with parameters and API key
-response = requests.get(url, params={
-    'apikey': api_key,
-    'keyword': artist_name,
-    'city': location
-})
-
-if response.status_code == 200:
-    events = response.json()
-    if '_embedded' in events and 'events' in events['_embedded']:
-        events = events['_embedded']['events']
-        for event in events:
-            print('Name:', event['name'])
-            print('Date:', event['dates']['start']['localDate'])
-            #print('Time:', event['dates']['start']['localTime'])
-            print('Venue:', event['_embedded']['venues'][0]['name'])
-            print('Location:', event['_embedded']['venues'][0]['city']['name'])
-            print('------------------------------')
+    if response.status_code == 200:
+        events = response.json()
+        event_list = [[]]
+        if '_embedded' in events and 'events' in events['_embedded']:
+            events = events['_embedded']['events']
+            
+            for event in events:
+                # print('Name:', event['name'])
+                # print('Date:', event['dates']['start']['localDate'])
+                # # print('Time:', event['dates']['start']['localTime'])
+                # print('Venue:', event['_embedded']['venues'][0]['name'])
+                # print('Location:', event['_embedded']['venues'][0]['city']['name'])
+                # print('------------------------------')
+                event_list.append([event['name'],event['dates']['start']['localDate'],event['_embedded']['venues'][0]['name'],event['_embedded']['venues'][0]['city']['name']])
+            
+        else:
+            print('No events found')
     else:
-        print('No events found')
-else:
-    print('Error:Request failed with status code', response.status_code)
+        print('Error:Request failed with status code', response.status_code)
+    return event_list[1:]
+# artist_name = input('Artist: ')
+print(events('Taylor Swift'))
