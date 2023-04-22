b3ae8f31e217a40681c2a6d32e9e002e3d91b8a6
KitYan20 <112986746+KitYan20@users.noreply.github.com>
2023-04-22 14:04:04 -0400

    Continuing to work on OAuth

--- static/script.js
+++ static/script.js
@@ -4,6 +4,10 @@ const resultsDiv = document.querySelector('#results');
 const addButton = document.querySelector('#add-button');
 const addInput = document.querySelector('#add-input');
 
+// var redirect_uri= "http://127.0.0.1:5000/register.html";
+// var client_id = "";
+// var client_secret = "";
+// const AUTHORIZE = "https://accounts.spotify.com/authorize"
 searchButton.addEventListener('click', async () => {
     const query = searchInput.value;
     console.log(query)
@@ -22,9 +26,20 @@ addButton.addEventListener('click', async () => {
     console.log(response.body)
 });
 
+// function requestAuthorization(){
+//     client_id = document.getElementById("clientId").value;
+//     client_secret = document.getElementById("clientSecret").value;
+//     localStorage.setItem("client_id", client_id);
+//     localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
 
-
-
+//     let url = AUTHORIZE;
+//     url += "?client_id=" + client_id;
+//     url += "&response_type=code";
+//     url += "&redirect_uri=" + encodeURI(redirect_uri);
+//     url += "&show_dialog=true";
+//     url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+//     window.location.href = url; // Show Spotify's authorization screen
+// }
 function displayResults(data) {
     
     const results = data;
@@ -41,3 +56,4 @@ function displayResults(data) {
     });
     resultsDiv.appendChild(ul);
 }
+
